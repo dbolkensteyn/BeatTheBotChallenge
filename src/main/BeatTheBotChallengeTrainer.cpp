a7c37b146d6373ec4c58fee2fd610399fdd89ec0
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2015-08-18 22:04:06 +0200

    Trainer: Add ability to capture videos from camera

--- src/main/BeatTheBotChallengeTrainer.cpp
+++ src/main/BeatTheBotChallengeTrainer.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
+#include <time.h>
 #include <sstream>
 #include "opencv2/opencv.hpp"
 
 #define WIDTH 640
 #define HEIGHT 512 // 0.8 * WIDTH
 
+static void printHelp()
+{
+  std::cout << "HELP: Set focus on the live video and press 'i' to capture an image, 'v' to start/end a video capture or 'q' to quit." << std::endl;
+}
+
 int main()
 {
   cv::VideoCapture cap(0);
-
   if (!cap.isOpened())
   {
-    std::cout << "Unable to open VideoCapture" << std::endl;
+    std::cout << "ERROR: Unable to open VideoCapture" << std::endl;
     return 1;
   }
 
@@ -19,23 +24,48 @@ int main()
   cap.set(CV_CAP_PROP_FRAME_HEIGHT, HEIGHT);
 
   std::cout << cv::getBuildInformation();
-  std::cout << "Webcam width: " << cap.get(CV_CAP_PROP_FRAME_WIDTH) << std::endl;
-  std::cout << "Webcam height: " << cap.get(CV_CAP_PROP_FRAME_HEIGHT) << std::endl;
-  std::cout << "Webcam FPS: " << cap.get(CV_CAP_PROP_FPS) << std::endl;
-  std::cout << "Webcam FOURCC: " << cap.get(CV_CAP_PROP_FOURCC) << std::endl;
-  std::cout << "Webcam Format: " << cap.get(CV_CAP_PROP_FORMAT) << std::endl;
 
-  cv::namedWindow("webcam", cv::WINDOW_AUTOSIZE);
-
+  bool videoCapturing = false;
+  time_t videoStart;
+  std::vector<cv::Mat> videoFrames;
   int counter = 0;
+
+  printHelp();
+  cv::namedWindow("live", cv::WINDOW_AUTOSIZE);
+
   while (true)
   {
     cv::Mat frame;
-	cap >> frame;
+    cap >> frame;
 
-    if (cv::waitKey(30) >= 0)
+    if (videoCapturing)
     {
-    	counter++;
+      videoFrames.push_back(frame);
+    }
+
+    int key = cv::waitKey(10);
+    if (key == 'q')
+    {
+      if (videoCapturing)
+      {
+        std::cout << "ERROR: Video capture is still on!" << std::endl;
+        printHelp();
+      }
+      else
+      {
+        break;
+      }
+    }
+    else if (key == 'i')
+    {
+      if (videoCapturing)
+      {
+        std::cout << "ERROR: Video capture is still on!" << std::endl;
+        printHelp();
+      }
+      else
+      {
+        counter++;
 
         std::stringstream ss;
         ss << "webcam_";
@@ -43,8 +73,51 @@ int main()
         ss << ".png";
         std::string path = ss.str();
 
-    	cv::imwrite(path, frame);
-    	std::cout << "Frame written to: " << path << std::endl;
+        cv::imwrite(path, frame);
+        std::cout << "Image written to: " << path << std::endl;
+      }
+    }
+    else if (key == 'v' && !videoCapturing)
+    {
+      videoCapturing = true;
+      time(&videoStart);
+      std::cout << "Video capture started!" << std::endl;
+    }
+    else if (key == 'v' && videoCapturing)
+    {
+      time_t videoEnd;
+      time(&videoEnd);
+      double fps = double(videoFrames.size()) / (videoEnd - videoStart);
+
+      counter++;
+
+      std::stringstream ss;
+      ss << "webcam_";
+      ss << counter;
+      ss << ".avi";
+      std::string path = ss.str();
+
+      cv::VideoWriter video(path, CV_FOURCC('X','V','I','D'), fps, cv::Size(WIDTH, HEIGHT));
+      if (!video.isOpened())
+      {
+        std::cout << "ERROR: Unable to open output video file: " << path << std::endl;
+        return 1;
+      }
+
+      for (std::vector<cv::Mat>::iterator it = videoFrames.begin(); it != videoFrames.end(); ++it)
+      {
+        video.write(*it);
+      }
+
+      std::cout << "Video at " << fps << " FPS written to: " << path << std::endl;
+
+      videoCapturing = false;
+      videoFrames.clear();
+    }
+    else if (key != -1)
+    {
+      std::cout << "Unknown command '" << (char)key << "'" << std::endl;
+      printHelp(); 
     }
 
     cv::imshow("live", frame);
